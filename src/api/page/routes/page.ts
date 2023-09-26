8a1456ec4e804a441e85fa45e91846b7569c921f
Anxo Rodriguez <anxolin@gmail.com>
2023-09-26 12:26:45 +0100

    Setup blog

---
+++ src/api/page/routes/page.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * page router.
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreRouter("api::page.page", {
+  config: {
+    find: {
+      middlewares: ["api::page.page-populate-middleware"],
+    },
+    findOne: {
+      middlewares: ["api::page.page-populate-middleware"],
+    },
+  },
+});
---
+++ src/api/page/services/page.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * page service.
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService("api::page.page");
