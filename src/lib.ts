02e2bd2299fd846fa93ad948cb23277313c39c77
Anxo Rodriguez <anxolin@gmail.com>
2024-04-17 14:23:32 +0100

    Publish client (#13)
    * Add client
    
    * Fix unused
    
    * Update version
    
    * Remove deprecated script and use prepare

--- src/lib.ts
+++ src/lib.ts
@@ -1,1 +1,18 @@
 export * from "./gen/types";
+
+import createClient from "openapi-fetch";
+import type { paths } from "./gen/types";
+
+export interface CmsClientOptions {
+  url?: string;
+}
+
+/**
+ * Open API Fetch client. See docs for usage https://openapi-ts.pages.dev/openapi-fetch/
+ */
+export function CmsClient(options: CmsClientOptions = {}) {
+  const { url = "https://cms.cow.fi/api" } = options;
+  return createClient<paths>({
+    baseUrl: url,
+  });
+}
