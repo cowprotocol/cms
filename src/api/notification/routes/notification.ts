6467b3dcc7ade4b860f9fa46d8e9f24126a814c8
shoom3301 <shoom3301@gmail.com>
2024-05-07 12:28:43 +0300

    feat: endpoint to get notifications of an account

--- src/api/notification/routes/notification.ts
+++ src/api/notification/routes/notification.ts
@@ -4,4 +4,41 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::notification.notification');
+const defaultRouter = factories.createCoreRouter('api::notification.notification');
+
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'GET',
+    path: '/notification-list/:account',
+    handler: 'notification.getNotificationList',
+    config: {
+      policies: [],
+      middlewares: [],
+    },
+  },
+  {
+    method: 'GET',
+    path: '/push-notifications/:account',
+    handler: 'notification.getPushNotifications',
+    config: {
+      policies: [],
+      middlewares: [],
+    },
+  }
+];
+
+export default customRouter(defaultRouter, myExtraRoutes);
--- src/api/notification/services/notification.ts
+++ src/api/notification/services/notification.ts
@@ -4,4 +4,45 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::notification.notification');
+const MODULE_ID = 'api::notification.notification'
+
+export default factories.createCoreService(MODULE_ID, ({ strapi }) => {
+  return {
+    async getNotificationList(account: string, push: boolean) {
+      const notifications = await strapi.entityService.findMany(
+        MODULE_ID,
+        {
+          start: 0,
+          limit: 50,
+          filters: {
+            account,
+            notification_template: { push }
+          },
+          populate: {
+            notification_template: {
+              fields: ['id', 'title', 'description', 'url', 'push', 'thumbnail']
+            }
+          },
+        }
+      )
+
+      return notifications.map(notification => ({
+        id: notification.id,
+        title: notification.notification_template.title,
+        description: templateNotification(notification.notification_template.description, notification.data),
+        url: notification.notification_template.url,
+        thumbnail: notification.notification_template.thumbnail
+      }))
+    },
+  }
+});
+
+function templateNotification(description: string, data: {[key: string]: string}): string {
+  let result = description
+
+  Object.keys(data).forEach(key => {
+    result = result.replace(`{{ ${key} }}`, data[key])
+  })
+
+  return result
+}
