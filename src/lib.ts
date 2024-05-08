69832ef727c4eb75090f57c5017c9a60ebed1579
Anxo Rodriguez <anxolin@gmail.com>
2024-05-08 15:30:05 +0300

    Allow to set your own headers and your API KEY

--- src/lib.ts
+++ src/lib.ts
@@ -5,14 +5,28 @@ import type { paths } from "./gen/types";
 
 export interface CmsClientOptions {
   url?: string;
+  apiKey?: string;
+  headers?: Record<string, string>;
 }
 
 /**
  * Open API Fetch client. See docs for usage https://openapi-ts.pages.dev/openapi-fetch/
  */
 export function CmsClient(options: CmsClientOptions = {}) {
-  const { url = "https://cms.cow.fi/api" } = options;
+  const {
+    url = "https://cms.cow.fi/api",
+    headers: baseHeaders = {},
+    apiKey,
+  } = options;
   return createClient<paths>({
     baseUrl: url,
+    headers: {
+      ...baseHeaders,
+      ...(apiKey
+        ? {
+            Authorization: "Bearer " + apiKey,
+          }
+        : {}),
+    },
   });
 }
