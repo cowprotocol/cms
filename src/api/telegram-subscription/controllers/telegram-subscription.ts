443f36b07d0ee4754e8d040db19bbe62a19134b5
shoom3301 <shoom3301@gmail.com>
2024-05-08 11:33:33 +0300

    feat: add telegram subscription

--- src/api/telegram-subscription/controllers/telegram-subscription.ts
+++ src/api/telegram-subscription/controllers/telegram-subscription.ts
@@ -3,5 +3,41 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { errors } from '@strapi/utils'
+import { TelegramData } from '../types'
 
-export default factories.createCoreController('api::telegram-subscription.telegram-subscription');
+const MODULE_ID = 'api::telegram-subscription.telegram-subscription'
+
+export default factories.createCoreController(MODULE_ID, ({strapi}) => {
+  return {
+    async addSubscription(context) {
+      const {account, data} : { account: string, data: TelegramData } = context.request.body
+
+      const service = strapi.service(MODULE_ID)
+
+      const existing = await strapi.entityService.findMany(MODULE_ID, { filters: { account, chat_id: data.id } })
+
+      if (existing.length > 0) return true
+
+      const result = await service.verifyTgAuthentication(data)
+
+      if (!result) {
+        throw new errors.ValidationError('Invalid telegram authentication data')
+      }
+
+      await service.addSubscription(account, data)
+
+      return true
+    },
+    async getSubscriptions(context) {
+      const account = context.params.account
+
+      return strapi.service(MODULE_ID).getAccountSubscriptions(account)
+    },
+    async sendNotifications(context) {
+      const account = context.params.account
+
+      return strapi.service(MODULE_ID).sendNotifications(account)
+    }
+  }
+});
