a4a1a78946dc7b754d52e22e0b5613e7b8a0d240
Anxo Rodriguez <anxolin@gmail.com>
2024-08-05 13:22:25 +0100

    Add solver information

---
+++ src/api/solver/controllers/solver.ts
@@ -0,0 +1,7 @@
+/**
+ * solver controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::solver.solver');
---
+++ src/api/solver/services/solver.ts
@@ -0,0 +1,7 @@
+/**
+ * solver service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::solver.solver');
