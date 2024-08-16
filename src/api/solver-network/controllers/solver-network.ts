ad2929575d65180b27dd0b7c2bfa099caf67fb61
Anxo Rodriguez <anxolin@gmail.com>
2024-08-16 17:25:27 +0100

    Remodel solvers (#44)
    * Remove addresses
    
    * Remodel solvers
    
    * Add environment

---
+++ src/api/solver-network/controllers/solver-network.ts
@@ -0,0 +1,7 @@
+/**
+ * solver-network controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::solver-network.solver-network');
---
+++ src/api/solver-network/routes/solver-network.ts
@@ -0,0 +1,7 @@
+/**
+ * solver-network router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::solver-network.solver-network');
