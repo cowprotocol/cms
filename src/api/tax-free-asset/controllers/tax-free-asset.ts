3db8cd73876311813abf61f0a41679738533fc33
Alexandr Kazachenko <shoom3301@gmail.com>
2024-12-12 18:46:48 +0500

    feat: add collection for assets that free from fees (#46)
    * feat: add collection for assets that free from fees
    
    * chore: fix chainId

---
+++ src/api/tax-free-asset/controllers/tax-free-asset.ts
@@ -0,0 +1,7 @@
+/**
+ * tax-free-asset controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tax-free-asset.tax-free-asset');
---
+++ src/api/tax-free-asset/routes/tax-free-asset.ts
@@ -0,0 +1,7 @@
+/**
+ * tax-free-asset router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tax-free-asset.tax-free-asset');
