e50d30a871a28e39ac81264b2e67d2a4057da4b0
Leandro <alfetopito@users.noreply.github.com>
2025-02-13 18:05:35 +0000

    feat: add Correlated Tokens (#54)

---
+++ src/api/correlated-token/controllers/correlated-token.ts
@@ -0,0 +1,7 @@
+/**
+ * correlated-token controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::correlated-token.correlated-token');
---
+++ src/api/correlated-token/routes/correlated-token.ts
@@ -0,0 +1,7 @@
+/**
+ * correlated-token router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::correlated-token.correlated-token');
