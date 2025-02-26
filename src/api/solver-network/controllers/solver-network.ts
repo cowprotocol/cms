af6d026cae7466fc775abd0830b4d7c48cd67c9a
= <bram@cow.fi>
2025-02-26 15:01:04 +0100

    Use Meilisearch for solver-network content type

--- src/api/solver-network/controllers/solver-network.ts
+++ src/api/solver-network/controllers/solver-network.ts
@@ -2,6 +2,16 @@
  * solver-network controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::solver-network.solver-network');
+export default factories.createCoreController('api::solver-network.solver-network', ({ strapi }) => ({
+  async search(ctx) {
+    try {
+      const queryParams = ctx.request.query;
+      const results = await strapi.service('api::solver-network.solver-network').search(queryParams);
+      return ctx.send(results);
+    } catch (error) {
+      return ctx.throw(500, error);
+    }
+  },
+}));
