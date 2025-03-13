c7509d31b57cf5e679a68b99e835d25f2232a860
Alfetopito <alfetopito@users.noreply.github.com>
2025-03-13 18:31:53 +0000

    refactor: move clean up task from cron file to a new service

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -4,49 +4,9 @@ export default {
    */
   cleanupExpiredNotifications: {
     task: async ({ strapi }) => {
-      console.log("Running cleanupExpiredNotifications task");
-      try {
-        // Calculate date 1 week ago
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-        // Find all notifications with templates due over a week ago
-        const expiredNotifications = await strapi.db
-          .query("api::notification.notification")
-          .findMany({
-            where: {
-              $or: [
-                {
-                  notification_template: {
-                    dueDate: {
-                      $lt: oneWeekAgo,
-                    },
-                  },
-                },
-                {
-                  notification_template: null,
-                },
-              ],
-            },
-          });
-
-        // Delete the expired and orphaned notifications
-        if (expiredNotifications.length > 0) {
-          await strapi.db.query("api::notification.notification").deleteMany({
-            where: {
-              id: {
-                $in: expiredNotifications.map((n) => n.id),
-              },
-            },
-          });
-          console.log(
-            `Deleted ${expiredNotifications.length} notifications that were over a week past due or orphaned`
-          );
-        }
-      } catch (error) {
-        console.error("Error cleaning up notifications:", error);
-      }
-      console.log("Finished cleanupExpiredNotifications task");
+      await strapi
+        .service("api::notification.cleanup")
+        .cleanupExpiredNotifications();
     },
     options: {
       rule: "0 0 * * *", // Runs daily at midnight
