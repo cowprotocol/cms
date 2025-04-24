20a11206aab590af96cfe09afa0c5a60a9c64766
Alexandr Kazachenko <shoom3301@gmail.com>
2025-04-24 13:12:36 +0100

    feat: check telegram subscriptions (#60)

--- src/api/telegram-subscription/controllers/telegram-subscription.ts
+++ src/api/telegram-subscription/controllers/telegram-subscription.ts
@@ -15,19 +15,39 @@ export default factories.createCoreController(MODULE_ID, ({strapi}) => {
 
       const service = strapi.service(MODULE_ID)
 
-      const existing = await strapi.entityService.findMany(MODULE_ID, { filters: { account, chatId: data.id } })
+      const isAlreadySubscribed = await this.checkSubscription(context)
 
-      if (existing.length > 0) return true
-
-      const result = await service.verifyTgAuthentication(data)
-
-      if (!result) {
-        throw new errors.ValidationError('Invalid telegram authentication data')
+      if (isAlreadySubscribed) {
+        return true
       }
 
       await service.addSubscription(account, data)
 
       return true
+    },
+    async checkSubscription(context) {
+      const {account, data} : { account: string, data: TelegramData } = context.request.body
+
+      const service = strapi.service(MODULE_ID)
+
+      const result = await service.verifyTgAuthentication(data)
+
+      /**
+       * Verify if the Telegram authentication data is valid
+       * Which proves that the data belongs to the user
+       */
+      if (!result) {
+        throw new errors.ValidationError('Invalid telegram authentication data')
+      }
+
+      const existing = await strapi.entityService.findMany(MODULE_ID, { filters: { account, chatId: data.id } })
+
+      /**
+       * We will only return true if the subscription belongs to the Telegram owner
+       * And the account is already subscribed
+       * So, it's not possible to check another account's subscription without owning Telegram account
+       */
+      return existing.length > 0
     },
     async getSubscriptions(context) {
       const { accounts } = context.query
