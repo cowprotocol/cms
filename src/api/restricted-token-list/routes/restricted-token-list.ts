4d57b120088d5016d91d4cc7bc55201dbdfeac0a
limitofzero <limitofzero@gmail.com>
2025-12-17 22:59:33 +0400

    feat: add restricted token lists page

---
+++ src/api/restricted-token-list/routes/restricted-token-list.ts
@@ -0,0 +1,8 @@
+/**
+ * restricted-token-list router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::restricted-token-list.restricted-token-list');
+
