83d754bee23172c04c1f0ab89412314d8cb859cd
Denis Makarov <limitofzero@gmail.com>
2025-12-18 18:31:01 +0400

    feat: add restricted token lists page (#72)

---
+++ src/api/restricted-token-list/controllers/restricted-token-list.ts
@@ -0,0 +1,8 @@
+/**
+ * restricted-token-list controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::restricted-token-list.restricted-token-list');
+
---
+++ src/api/restricted-token-list/services/restricted-token-list.ts
@@ -0,0 +1,8 @@
+/**
+ * restricted-token-list service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::restricted-token-list.restricted-token-list');
+
