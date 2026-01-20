e80f200981dd0c1aaffeab027ca26e76f9839943
Daniel Constantin <hello@danielconstantin.net>
2026-01-20 15:08:55 +0200

    chore: update core offer

--- config/affiliate.ts
+++ config/affiliate.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   defaults: {
-    rewardAmount: env("AFFILIATE_REWARD_AMOUNT", "10"),
+    rewardAmount: env("AFFILIATE_REWARD_AMOUNT", "20"),
     triggerVolume: env("AFFILIATE_TRIGGER_VOLUME", "250000"),
     timeCapDays: env("AFFILIATE_TIME_CAP_DAYS", "90"),
     volumeCap: env("AFFILIATE_VOLUME_CAP", "0"),
